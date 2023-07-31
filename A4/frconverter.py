da845511b0e6876e8d87f1d280b5283edeb83149
Alex <53021768+GhostDaGhost@users.noreply.github.com>
2023-07-31 13:29:57 -0700

    modify(A4): Update FR converter

--- A4/frconverter.py
+++ A4/frconverter.py
@@ -1,25 +1,33 @@
-# Author: Alex Arizola
-# Program 1 / Fahrenheit to Reaumur Converter
+# Author: Alexander Arizola
+# A4 - Fahrenheit to Reaumur Converter Function
+# This is a program that will convert a Fahrenheit temperature value to Reaumur.
 
-# WHEN TRIGGERED, CONVERT THE FAHRENHEIT INPUT TO REAUMUR 
-def ConvertFahrenheitToReaumur(fahrenheitInput):
-    if fahrenheitInput == '': # PARAMETER CHECK
-        return print('[ConvertFahrenheitToReaumur]: Did not receive a valid parameter.')
+# WHEN TRIGGERED, THIS WILL ASK THE USER TO INPUT A FAHRENHEIT TEMPERATURE VALUE FOR CONVERSION LATER
+# No parameters
+# No return value
+def OnProgramExecution():
+    fahrenheitInput = int(input('Enter a temperature in fahrenheit: ')) # ASK FOR INPUT
 
-    # CONTINUE THE FUNCTION
-    reaumurResult = (fahrenheitInput - 32) * (4 / 9)
-    print('Reaumur: ', reaumurResult)
-
-# WHEN TRIGGERED, ASK USER FOR FAHRENHEIT THEY'D LIKE TO CONVERT
-def AskUserForFahrenheit():
-    fahrenheitInput = input('Enter a temperature in fahrenheit: ')
-    
-    print('You have entered: ', fahrenheitInput)
-    if fahrenheitInput != '':
-        ConvertFahrenheitToReaumur(int(fahrenheitInput))
+    # FEEDBACK PRINT AND CHECK IF IT IS VALID
+    print('[OnProgramExecution]: You have entered a fahrenheit temperature of: ', str(fahrenheitInput))
+    if fahrenheitInput and fahrenheitInput > 0:
+        FR_converter(fahrenheitInput)
     else:
-        print('You did not enter a correct input!')
+        print('[OnProgramExecution]: You did not enter a correct input! Please try again.')
 
+# WHEN TRIGGERED, THIS WILL CONVERT AN INPUTTED FAHRENHEIT TEMPERATURE VALUE INTO REAUMUR
+# @param fahrenheitToConvert {int} - The fahrenheit temperature to convert into reaumur
+# @return reaumurResult {str} - The converted temperature in reaumur
+def FR_converter(fahrenheitToConvert):
+    if not fahrenheitToConvert or fahrenheitToConvert <= 0: # PARAMETER CHECK
+        return print('[FR_converter]: Did not receive a valid parameter. Please try again!')
 
-# TRIGGERS UPON SCRIPT EXECUTION
-AskUserForFahrenheit()
+    # CONVERT FAHRENHEIT INTO REAUMUR
+    reaumurResult = format((fahrenheitToConvert - 32) * (4 / 9), '.2f')
+    print('[FR_converter]: Reaumur: ', reaumurResult)
+
+    # RETURN CONVERTED VALUE
+    return reaumurResult
+
+# TRIGGER THE MAIN FUNCTION UPON PROGRAM EXECUTION
+OnProgramExecution()
